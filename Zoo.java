0bf0f6dde99eb70967b47b66e0f77d03c10fc743
fedibenman <fedibenmansour7@gmail.com>
2024-09-24 17:18:48 +0100

    prosite 3

--- Zoo.java
+++ Zoo.java
@@ -2,7 +2,8 @@ public class Zoo{
   public Animal [] animals ; 
   public String name ; 
   public String city ; 
-  public int nbrCages ; 
+  public final int nbrCages ; 
+  public int nbAnimal = 0 ; 
 
   public Zoo(String name,String city, int nbrCages){
     animals = new Animal[25] ;
@@ -14,5 +15,27 @@ public class Zoo{
 
   public String toString(){ 
     return name + " " + city + " " + nbrCages ; 
+  }
+
+
+  public boolean addAnimal(Animal animal){
+    
+    if (nbAnimal+1 > nbrCages)
+    return false  ;
+    if (searchAnimal(animal) != -1)
+    return false  ; 
+    animals[nbAnimal] = animal ; 
+    nbAnimal++ ;
+    return true  ;
+  }
+
+  int searchAnimal(Animal animal){
+
+    for (int i = 0 ; i < nbAnimal ; i++){
+      System.err.println("the int" + i + "animal" + animals[i]);
+      if (animals[i].name.equals(animal.name))
+      return i ;
+    }
+    return -1 ;
   }
 }
--- ZooManagement.java
+++ ZooManagement.java
@@ -3,7 +3,9 @@ public class ZooManagement{
     public static void main(String args[]){
 Animal lion = new Animal("cats", "lion", 15, true);
 Zoo myzoo = new Zoo("myzoo", "Bizerte", 10);     
-myzoo.animals[0] = lion;
+for (int i = 0 ; i< 15 ;i++){
+System.out.println(myzoo.addAnimal(lion));
+}
 System.err.println(lion.toString());
 System.out.println(myzoo.toString());
 }
